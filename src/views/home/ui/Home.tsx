5f61473326489316161bebd5974d40cf146006b8
nataliaN <asdfghmax@yandex.ru>
2026-01-27 14:48:36 +0300

    refactor: add common icons

--- src/views/home/ui/Home.tsx
+++ src/views/home/ui/Home.tsx
@@ -1,12 +1,55 @@
 import Link from 'next/link';
 
+import {
+  BraveIcon,
+  ChromeIcon,
+  ExplorerIcon,
+  FacebookIcon,
+  FirefoxIcon,
+  FlagRussiaIcon,
+  FlagUKIcon,
+  GoogleIcon,
+  MicrosoftEdgeIcon,
+  OperaIcon,
+  PaidIcon,
+  PayPalIcon,
+  RecaptchaIcon,
+  SafariIcon,
+  StripeIcon,
+  UcBrowserIcon,
+  YandexIcon,
+} from '@/shared/icons/common';
+import {GitHubDarkIcon} from '@/shared/icons/dark-theme';
+
 import s from './Home.module.scss';
 
 const HomeView = () => {
   return (
     <div className={s.page}>
-      <Link href={'/profile'}>Profile</Link>
-      <Link href={'/settings'}>Settings</Link>
+      <div style={{display: 'flex', gap: '20px'}}>
+        <Link href={'/profile'}>Profile</Link>
+        <Link href={'/settings'}>Settings</Link>
+      </div>
+      <div style={{display: 'flex', gap: '20px', flexWrap: 'wrap'}}>
+        <FlagRussiaIcon />
+        <FlagUKIcon />
+        <FacebookIcon />
+        <GitHubDarkIcon />
+        <GoogleIcon />
+        <PaidIcon />
+        <PayPalIcon />
+        <RecaptchaIcon />
+        <StripeIcon />
+        <ChromeIcon />
+        <OperaIcon />
+        <FirefoxIcon />
+        <ExplorerIcon />
+        <SafariIcon />
+        <BraveIcon />
+        <MicrosoftEdgeIcon />
+        <YandexIcon />
+        <UcBrowserIcon />
+      </div>
     </div>
   );
 };
