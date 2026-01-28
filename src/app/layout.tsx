3b827507d212446a2631b10c8dddc49c67fbb5cf
pashz7 <pashec1710@gmail.com>
2026-01-28 20:49:09 +0300

    style(scss): add layout and responsive mixins and variables

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ const inter = Inter({
   subsets: ['latin', 'cyrillic'],
   weight: ['400', '500', '600', '700'],
   display: 'swap',
-  variable: '--font-sans',
+  variable: '--font-primary',
 });
 
 export const metadata: Metadata = {
