4558eeadf09600361c4f2ebea64a6d48abe64531
pashz7 <pashec1710@gmail.com>
2026-01-28 16:49:30 +0300

    fix: make font variable global and simplify html styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,8 +27,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.variable}>
+    <html lang="en" className={inter.variable}>
+      <body>
         <div id="root">
           <Header />
           <main>{children}</main>
