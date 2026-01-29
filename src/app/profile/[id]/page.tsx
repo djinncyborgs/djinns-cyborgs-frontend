910fd4b48e9f2e5ed1600982e3f2a8471026c3db
djinncyborgs <djinn.cyborgs@mail.ru>
2026-01-29 23:07:22 +0300

    test merge  (#14)
    * feat: implement routing
    
    * refactor: change structure of the project
    
    * refactor: rename folders and components
    
    * refactor: add common icons
    
    * refactor: remove redirect from profile route
    
    * refactor: change structure of the icons folder
    
    * refactor: add icons
    
    * project-documentation (#8)
    
    * docs: update CONTRIBUTING and README
    
    * docs: update Git Flow structure in CONTRIBUTING (RU/EN)
    
    * docs: add icon to git flow section and fix README's
    
    * refactor: routing
    
    ---------
    
    Co-authored-by: nataliaN <asdfghmax@yandex.ru>
    Co-authored-by: Dzmitry Herashchanka <78657444+FeHixX@users.noreply.github.com>

---
+++ src/app/profile/[id]/page.tsx
@@ -0,0 +1,24 @@
+import {Metadata} from 'next';
+import {redirect} from 'next/navigation';
+
+import UserProfileView from '@/views/userProfile';
+
+export const metadata: Metadata = {
+  title: 'User Profile Page',
+  description: 'Profile Page',
+};
+type Props = {
+  params: Promise<{id: string}>;
+  searchParams: Promise<Record<string, string>>;
+};
+
+export default async function UserProfile({params, searchParams}: Props) {
+  const {id} = await params;
+  const {postId, action} = await searchParams;
+
+  if (postId && action) {
+    redirect(`/profile/${id}?postId=${postId}`);
+  }
+
+  return <UserProfileView id={id} />;
+}
